43ae6d908224b9fa42ae191fb645ed007bf612f0
leedayeong <98dlstod@naver.com>
2022-07-24 22:38:26 +0900

    BE - Swagger Setting

---
+++ BackEnd/octopus/src/main/java/com/ssafy/octopus/OctopusApplication.java
@@ -0,0 +1,14 @@
+package com.ssafy.octopus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OctopusApplication {
+
+	public static void main(String[] args) {
+		System.out.print("hello world - dayeong- ");
+		SpringApplication.run(OctopusApplication.class, args);
+	}
+}
+
