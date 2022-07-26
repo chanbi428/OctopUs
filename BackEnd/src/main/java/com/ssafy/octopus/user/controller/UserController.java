01db52d15854e46819270bde70110842662752f9
손준혁 <sonjuhy@gmail.com>
2022-07-26 23:03:16 +0900

    FEAT / REQ-01-01-BE / SJH

---
+++ BackEnd/src/main/java/com/ssafy/octopus/user/controller/UserController.java
@@ -0,0 +1,52 @@
+package com.ssafy.octopus.user.controller;
+
+import com.ssafy.octopus.user.entity.User;
+import com.ssafy.octopus.user.entity.UserDto;
+import com.ssafy.octopus.user.service.UserService;
+import com.ssafy.octopus.user.service.UserServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+/*
+ * Write by SJH
+ * */
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService service = new UserServiceImpl();
+
+    @Operation(summary = "Sign Up/IN", description = "sign in / sign up api")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK !!"),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST !!"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND !!"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR !!")
+    })
+    @GetMapping("/api")
+    public void api(){
+
+    }
+
+
+    @PostMapping("/SignIn") // 회원가입
+    public ResponseEntity<User> signIn(@RequestBody UserDto dto){
+        User user = service.save(dto);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/secession") // 회원 탈퇴
+    public ResponseEntity<Boolean> secession(@RequestBody UserDto dto){
+        boolean result = service.delete(dto.getIdx()); // fix here : jpa delete return void
+        if(result) return new ResponseEntity<>(result, HttpStatus.OK);
+        else return new ResponseEntity<>(result, HttpStatus.NOT_FOUND);
+    }
+}
---
+++ BackEnd/src/main/java/com/ssafy/octopus/user/service/UserServiceImpl.java
@@ -0,0 +1,48 @@
+package com.ssafy.octopus.user.service;
+
+import com.ssafy.octopus.user.entity.User;
+import com.ssafy.octopus.user.entity.UserDto;
+import com.ssafy.octopus.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/*
+ * Write by SJH
+ * */
+@Service
+public class UserServiceImpl implements UserService{
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public User save(UserDto userDto) {
+        return repository.save(userDto);
+    }
+
+    @Override
+    public boolean delete(int idx) {
+        return repository.deleteByIdx(idx);
+    }
+
+    @Override
+    public boolean idOverlapCheck(String userId) {
+        int result = repository.existsByUserId(userId);
+        if(result != 0) return false;
+        else return true;
+    }
+
+    @Override
+    public boolean pwOverlapCheck(String userPw) {
+        int result = repository.existsByUserPw(userPw);
+        if(result != 0) return false;
+        return true;
+    }
+
+    @Override
+    public boolean nameOverlapCheck(String userName) {
+        int result = repository.existsByUserName(userName);
+        if(result != 0) return false;
+        return true;
+    }
+}
