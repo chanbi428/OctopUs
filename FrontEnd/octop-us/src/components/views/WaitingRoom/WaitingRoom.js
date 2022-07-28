e3c0146d13f534da7796618d1f805013ac0fdca8
이승연 <tmddus3631@naver.com>
2022-07-28 15:01:33 +0900

    #[REQ-06-01] fix : 오타 수정

--- FrontEnd/octop-us/src/components/views/WaitingRoom/WaitingRoom.js
+++ FrontEnd/octop-us/src/components/views/WaitingRoom/WaitingRoom.js
@@ -2,46 +2,34 @@ import React, {useState} from 'react';
 import axios from "axios";
 // import MakeRoom from './MakeRoom/MakeRoom'
 
-// gameStatus": true,
-//   "gameTime": 0,
-//   "idx": 0,
-//   "personLimit": 0,
-//   "personNum": 0,
-//   "private": true,
-//   "roomChief": "string",
-//   "roomId": "string",
-//   "roomName": "string",
-//   "roomPw": "string",
-//   "userList": "string"
-// }
-
 export default function WaitingRoom() {
-	const [gameTime, setGameTime] = useState()
-	const [idx, setIdx] = useState()
-	const [personLimit, setPersonLimit] = useState()
-	const [personNum, setPersonNum] = useState()
-	const [isPrivate, setIsPrivate] = useState()
-	const [roomChief, setRoomChief] = useState()
-	const [roomId, setRoomId] = useState()
-	const [roomName, setRoomName] = useState()
-	const [roomPw, setRoomPw] = useState()
-	const [userList, setUserList] = useState()
+    const [gameStatus, setGameStatus] = useState(false)
+	const [gameTime, setGameTime] = useState("")
+	const [idx, setIdx] = useState("")
+	const [personLimit, setPersonLimit] = useState("")
+	const [personNum, setPersonNum] = useState("")
+	const [isPrivate, setIsPrivate] = useState("")
+	const [roomChief, setRoomChief] = useState("")
+	const [roomId, setRoomId] = useState("")
+	const [roomName, setRoomName] = useState("")
+	const [roomPw, setRoomPw] = useState("")
+	const [userList, setUserList] = useState([])
 
 	const exitRoom = async() => {
-		const getRoomInfo = await axios.get(`http://localhot:8080/rooms/detail/roomid/${roomId}`)
+        const getRoomInfo = await axios.get(`http://localhost:8080/rooms/detail/roomid/${roomId}`)
         console.log(getRoomInfo)
-        const usersInfo = await getRoomInfo.userList.splice(',')
-		updateRoomInfo(getRoomInfo.gameTime, getRoomInfo.idx, getRoomInfo.personLimit, getRoomInfo.personNum, getRoomInfo.isPrivate, getRoomInfo.roomChief, getRoomInfo.roomId, getRoomInfo.roomName, getRoomInfo.roomPw, getRoomInfo.usersInfo)
-		let userListInfo = userList
+        const usersInfo = await getRoomInfo.userList.split(',')
+        updateRoomInfo(getRoomInfo.gameStatus, getRoomInfo.gameTime, getRoomInfo.idx, getRoomInfo.personLimit, getRoomInfo.personNum, getRoomInfo.isPrivate, getRoomInfo.roomChief, getRoomInfo.roomId, getRoomInfo.roomName, getRoomInfo.roomPw, getRoomInfo.usersInfo)
+        let userListInfo = userList
         let roomChiefInfo = roomChief
-		if (personNum === 1) {
-				axios.delete(`http://localhost:8080/rooms/${roomId}`)
-				// 소켓으로 방 탈출 요청
-		} else if (personNum >= 2) {
+        if (personNum === 1) {
+                axios.delete(`http://localhost:8080/rooms/${roomId}`)
+                // 소켓으로 방 탈출 요청
+        } else if (personNum >= 2) {
             if ('userName' === roomChief) {
                 // 유저 닉네임 정보 필요함
                 for (let user of userListInfo) {
-                    if (user != 'userName' && user != "") {
+                    if (!user === 'userName' && !user === "") {
                         roomChiefInfo = user
                         break;
                     }
@@ -49,8 +37,9 @@ export default function WaitingRoom() {
             }
             userListInfo.splice(userListInfo.indexOf('userName'), 1)
             let personNumInfo = personNum - 1
-            updateRoomInfo(gameTime, idx, personLimit, personNum, isPrivate, roomChiefInfo, roomId, roomName, roomPw, userListInfo)
+            updateRoomInfo(gameStatus, gameTime, idx, personLimit, personNum, isPrivate, roomChiefInfo, roomId, roomName, roomPw, userListInfo)
             const data = {
+                gameStatus: gameStatus,
                 gameTime : gameTime,
                 idx : idx,
                 personLimit : personLimit,
@@ -63,14 +52,15 @@ export default function WaitingRoom() {
             }
             const updateRoom = await axios.put('http://localhost:8080/rooms', data=data)
             console.log(data)
-		}
+        }
         // 소켓으로 방 나감 알림
-
+	
 	}
 
 
-	const updateRoomInfo = (gameTime, idx, personLimit, personNum, isPrivate, roomChief, roomId, roomName, roomPw, userList) =>{
-		setGameTime(gameTime)
+	const updateRoomInfo = (gameStatus, gameTime, idx, personLimit, personNum, isPrivate, roomChief, roomId, roomName, roomPw, userList) =>{
+		setGameStatus(gameStatus)
+        setGameTime(gameTime)
 		setIdx(idx)
 		setPersonLimit(personLimit)
 		setPersonNum(personNum)
@@ -91,7 +81,7 @@ export default function WaitingRoom() {
     const startGame = async () => {
         const getRoomInfo = await axios.get(`http://localhost:8080/rooms/detail/roomid/${roomId}`)
         console.log(getRoomInfo)
-        const users = getRoomInfo.userList.splice(',')
+        const users = getRoomInfo.userList.split(',')
         for (let user of users) {
             let cnt = 0
             if (user === "") {
