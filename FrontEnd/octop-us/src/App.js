d6e966d89ae4253bb8c1d49cc4c7099663414915
leedayeong <98dlstod@naver.com>
2022-07-29 23:31:06 +0900

    #[S07P12E106-84] FEAT/REQ-03-BE/LDY

--- FrontEnd/octop-us/src/App.js
+++ FrontEnd/octop-us/src/App.js
@@ -1,12 +1,13 @@
 // import logo from "./logo.svg";
 import "./App.css";
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom";
 import LandingPage from "./components/views/LandingPage/LandingPage";
-import MainPage from "./components/views/MainPage/MainPage"
-import Tmp from './components/views/tmp/tmp'
+import MainPage from "./components/views/MainPage/MainPage";
+import Tmp from "./components/views/tmp/tmp";
 // import LoginForm from "./components/views/LandingPage/LoginForm";
 import WaitingRoom from "./components/views/WaitingRoom/WaitingRoom";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import VideoRoomComponent from "./components/views/VideoChat/VideoRoomComponent";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/main" element={<MainPage />} />
         <Route path="/tmp" element={<Tmp />} />
         <Route path="/wait" element={<WaitingRoom />}></Route>
+        <Route path="/video" element={<VideoRoomComponent />} />
       </Routes>
     </div>
   );
