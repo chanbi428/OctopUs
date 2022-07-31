3f0792946d4cb57bf65f40ab2a5a195618083193
leedayeong <98dlstod@naver.com>
2022-07-31 11:18:39 +0900

    #[S07P12E106-68] FEAT/REQ-18-03/BE/LDY

--- BackEnd/src/main/java/com/ssafy/octopus/ingame/dao/GamerDao.java
+++ BackEnd/src/main/java/com/ssafy/octopus/ingame/dao/GamerDao.java
@@ -2,11 +2,61 @@ package com.ssafy.octopus.ingame.dao;
 
 import com.ssafy.octopus.ingame.entity.Gamer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface GamerDao extends JpaRepository<Gamer, Integer> {
 
     public Gamer findByUserName(String userName);
+
+
+    /** @brief : findByRoomId, (roomId)가 일치하는 Gamer 들을 찾아줌
+     *  @date : 2022-07-31
+     *  @param : roomId
+     *  @return : List<Gamer>
+     *  @author : LDY, 98dlstod@naver.com
+     */
+    List<Gamer> findByRoomId(String roomId);
+
+
+    /** @brief : updateByUserName, userName에 해당하는 게이머 승리로 변경
+     *  @date : 2022-07-31
+     *  @param : userName
+     *  @return : int
+     *  @author : LDY, 98dlstod@naver.com
+     */
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE gamer SET is_victory = 1 WHERE user_name = ?", nativeQuery = true)
+    int updateByUserName(String userName);
+
+    /** @brief : updateByGameTeam, gameTeam에 해당하는 게이머들 승리로 변경
+     *  @date : 2022-07-31
+     *  @param : gameTeam
+     *  @return : int
+     *  @author : LDY, 98dlstod@naver.com
+     */
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE gamer SET is_victory = 1 WHERE game_team = ?", nativeQuery = true)
+    int updateByGameTeam(String gameTeam);
+
+
+//    /** @brief : Gamer List, 모든 Gamer List 반환
+//     *  @date : 2022-07-31
+//     *  @param
+//     *  @return : ResponseEntity<List<Cave>>
+//     *  @author : LDY, 98dlstod@naver.com
+//     */
+//    @Transactional
+//    Long deleteByRoomId(String roomId);
+
+
 
 }
--- BackEnd/src/main/java/com/ssafy/octopus/ingame/service/GamerServiceImpl.java
+++ BackEnd/src/main/java/com/ssafy/octopus/ingame/service/GamerServiceImpl.java
@@ -5,6 +5,9 @@ import com.ssafy.octopus.ingame.entity.Gamer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class GamerServiceImpl implements GamerService {
 
@@ -25,5 +28,71 @@ public class GamerServiceImpl implements GamerService {
             return  gamer;
         }
     }
+
+    /** @brief : Gamer List, 모든 Gamer List 반환
+     *  @date : 2022-07-31
+     *  @param
+     *  @return : List<Gamer>
+     *  @author : LDY, 98dlstod@naver.com
+     */
+    public  List<Gamer> list(){
+        return dao.findAll();
+    }
+
+    /** @brief : findByRoomId, (roomId)가 일치하는 Gamer 들을 찾아줌
+     *  @date : 2022-07-31
+     *  @param : roomId
+     *  @return : List<Gamer>
+     *  @author : LDY, 98dlstod@naver.com
+     */
+    @Override
+    public List<Gamer> findByRoomId(String roomId){
+        return dao.findByRoomId(roomId);
+    }
+
+
+    /** @brief : isDead, userName에 해당하는 게이머의 생사 확인
+     *  @date : 2022-07-31
+     *  @param : userName
+     *  @return : Boolean
+     *  @author : LDY, 98dlstod@naver.com
+     */
+    @Override
+    public Boolean isDead(String userName) {
+        Gamer gamer = findByUserName(userName);
+        if(gamer == null) return false;
+        return gamer.isDead() ? true : false;
+    }
+
+    /** @brief : updateByUserName, userName에 해당하는 게이머 승리로 변경
+     *  @date : 2022-07-31
+     *  @param : userName
+     *  @return : int
+     *  @author : LDY, 98dlstod@naver.com
+     */
+    @Override
+    public int updateByUserName(String userName) {
+        return dao.updateByUserName(userName);
+    }
+
+    /** @brief : updateByGameTeam, gameTeam에 해당하는 게이머들 승리로 변경
+     *  @date : 2022-07-31
+     *  @param : gameTeam
+     *  @return : int
+     *  @author : LDY, 98dlstod@naver.com
+     */
+    @Override
+    public int updateByGameTeam(String gameTeam) {
+        return dao.updateByGameTeam(gameTeam);
+    }
+
+//    /** @brief : deleteCaves, 해당하는 roomId를 가진 cave들 삭제
+//     *  @date : 2022-07-31
+//     *  @param : RoomId
+//     *  @return : ResponseEntity<Long>
+//     *  @author : LDY, 98dlstod@naver.com
+//     */
+//    @Override
+//    public Long deleteByRoomId(String roomId) {return dao.deleteByRoomId(roomId);}
 
 }
