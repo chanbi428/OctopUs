37e152bd1d6f547664e11aa3596de7e89027faa8
leedayeong <98dlstod@gmail.com>
2022-08-04 22:15:07 +0900

    #[S07P12E106-64] FEAT/REDUX SETTING/FE/LDY

--- FrontEnd/octop-us/src/app/store.js
+++ FrontEnd/octop-us/src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/user/userSlice";
+import gamerReducer from "../features/gamer/gamerSlice";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    gamer: gamerReducer,
   },
 });
 
