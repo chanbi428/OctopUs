f9c9c690cc0eedaaf08a7dd030b066268e1ef639
Chanbi Bae <73926213+chanbi428@users.noreply.github.com>
2022-08-08 14:12:46 +0900

    feat : 마피아, 유령 채팅 설정

--- FrontEnd/octop-us/src/components/InGame/openVidu/chat/ChatComponent.js
+++ FrontEnd/octop-us/src/components/InGame/openVidu/chat/ChatComponent.js
@@ -11,6 +11,7 @@ import { connect } from 'react-redux';
 // import axios from "axios";
 // import { room } from "../../../../features/waiting/waitSlice"
 // import { gamerInit, gamerUserList } from "../../../../features/gamer/gamerActions"
+import { setMessageList } from "../../../../features/gamer/gamerSlice";
 
 class ChatComponent extends Component {
   constructor(props) {
@@ -35,12 +36,14 @@ class ChatComponent extends Component {
       .getStreamManager()
       .stream.session.on("signal:chat", (event) => {
         const data = JSON.parse(event.data);
-        let messageList = this.state.messageList;
-        messageList.push({
+        let message = {
           connectionId: event.from.connectionId,
           nickname: data.nickname,
           message: data.message,
-        });
+          isDead: data.isDead,
+          job: data.job,
+          gameStatus: data.gameStatus,
+        };
         const document = window.document;
         setTimeout(() => {
           const userImg = document.getElementById(
@@ -50,7 +53,21 @@ class ChatComponent extends Component {
           const avatar = userImg.getContext("2d");
           avatar.drawImage(video, 200, 120, 285, 285, 0, 0, 60, 60);
         }, 50);
-        this.setState({ messageList: messageList });
+        if (data.isDead == true && this.props.gamerData.isDead == true) {
+          // 유령
+          this.props.setMessageList({ message: message });
+        }
+        if (data.isDead === false && this.props.gamerData.gameStatus !== 1) {
+          this.props.setMessageList({ message: message });
+        } else if (
+          this.props.gamerData.job === "마피아" &&
+          this.props.gamerData.gameStatus === 1 &&
+          this.props.gamerData.isDead === false &&
+          data.job === "마피아" &&
+          data.isDead === false
+        ) {
+          this.props.setMessageList({ message: message });
+        }
         this.scrollToBottom();
       });
   }
@@ -74,6 +91,9 @@ class ChatComponent extends Component {
           message: message,
           nickname: this.props.user.getNickname(),
           streamId: this.props.user.getStreamManager().stream.streamId,
+          isDead: this.props.gamerData.isDead, // 메세지 보낼 때 생사도 함께 보낸다.
+          job: this.props.gamerData.job,
+          gameStatus: this.props.gamerData.gameStatus,
         };
         this.props.user.getStreamManager().stream.session.signal({
           data: JSON.stringify(data),
@@ -99,7 +119,7 @@ class ChatComponent extends Component {
 
   enterNotice() {
     console.log("입장 알림")
-    const data = { message: `[알림] 누구님이 입장하셨습니다`, nickname: '서버', streamId: this.props.user.getStreamManager().stream.streamId };
+    const data = { message: `[알림] ${this.props.userData.userInfo.userName}님이 입장하셨습니다`, nickname: '서버', streamId: this.props.user.getStreamManager().stream.streamId, job: '', gameStatus: 0 , isDead: false };
     this.props.user.getStreamManager().stream.session.signal({
         data: JSON.stringify(data),
         type: 'chat',
@@ -112,7 +132,7 @@ class ChatComponent extends Component {
 
   exitNotice() {
     console.log("입장 알림")
-    const data = { message: `[알림] 누구님이 퇴장하셨습니다`, nickname: '서버', streamId: this.props.user.getStreamManager().stream.streamId };
+    const data = { message: `[알림] ${this.props.userData.userInfo.userName}님이 퇴장하셨습니다`, nickname: '서버', streamId: this.props.user.getStreamManager().stream.streamId, job: '', gameStatus: 0 , isDead: false };
     this.props.user.getStreamManager().stream.session.signal({
         data: JSON.stringify(data),
         type: 'chat',
@@ -121,7 +141,7 @@ class ChatComponent extends Component {
 
   gameNotice() {
     console.log("게임 시작 알림")
-    const data = { message: `[게임] 게임을 시작합니다`, nickname: '서버', streamId: this.props.user.getStreamManager().stream.streamId };
+    const data = { message: `[게임] 게임을 시작합니다`, nickname: '서버', streamId: this.props.user.getStreamManager().stream.streamId, job: '', gameStatus: 0 , isDead: false };
     this.props.user.getStreamManager().stream.session.signal({
         data: JSON.stringify(data),
         type: 'chat',
@@ -152,28 +172,92 @@ class ChatComponent extends Component {
             </IconButton>
           </div>
           <div className="message-wrap" ref={this.chatScroll}>
-            {this.state.messageList.map((data, i) => (
-              <div
-                key={i}
-                id="remoteUsers"
-                className={
-                  "message" +
-                  (data.connectionId !== this.props.user.getConnectionId()
-                    ? " left"
-                    : " right")
-                }
-              >
-                <div className="msg-detail">
-                  <div className="msg-info">
-                    <p> {data.nickname}</p>
+          {gamerData.messageList.map((data, i) => {
+              if (gamerData.isDead === true && data.isDead === true) {
+                // 유령들끼리만 보이는 채팅 (여기 글자색을 다르게 한다던지 하면될듯)
+                return (
+                  <div
+                    key={i}
+                    id="remoteUsers"
+                    className={
+                      "message" +
+                      (data.connectionId !== this.props.user.getConnectionId()
+                        ? " left"
+                        : " right" + " ghostColor")
+                    }
+                  >
+                    <canvas id={"userImg-" + i} width="60" height="60" className="user-img" />
+                    <div className="msg-detail">
+                      <div className="msg-info">
+                        <p> {data.nickname}</p>
+                      </div>
+                      <div className="msg-content">
+                        <span className="triangle" />
+                        <p className="ghost">{data.message}</p>
+                      </div>
+                    </div>
                   </div>
-                  <div className="msg-content">
-                    <span className="triangle" />
-                    <p className="text">{data.message}</p>
+                );
+              }
+              if (data.isDead === false && data.gameStatus !== 1) {
+                // 살아있는 사람의 채팅은 모두에게 보인다.
+                return (
+                  <div
+                    key={i}
+                    id="remoteUsers"
+                    className={
+                      "message" +
+                      (data.connectionId !== this.props.user.getConnectionId()
+                        ? " left"
+                        : " right" + " alive")
+                    }
+                  >
+                    <canvas id={"userImg-" + i} width="60" height="60" className="user-img" />
+                    <div className="msg-detail">
+                      <div className="msg-info">
+                        <p> {data.nickname}</p>
+                      </div>
+                      <div className="msg-content">
+                        <span className="triangle" />
+                        <p className="text">{data.message}</p>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
-            ))}
+                );
+              }
+              if (
+                gamerData.job === "마피아" &&
+                gamerData.isDead === false &&
+                data.job === "마피아" &&
+                data.isDead === false &&
+                data.gameStatus === 1
+              ) {
+                // 밤이 1라는 가정
+                return (
+                  <div
+                    key={i}
+                    id="remoteUsers"
+                    className={
+                      "message" +
+                      (data.connectionId !== this.props.user.getConnectionId()
+                        ? " left"
+                        : " right" + " mafiaColor")
+                    }
+                  >
+                    <canvas id={"userImg-" + i} width="60" height="60" className="user-img" />
+                    <div className="msg-detail">
+                      <div className="msg-info">
+                        <p> {data.nickname}</p>
+                      </div>
+                      <div className="msg-content">
+                        <span className="triangle" />
+                        <p className="mafia">{data.message}</p>
+                      </div>
+                    </div>
+                  </div>
+                );
+              }
+            })}
           </div>
 
           <div id="messageInput">
@@ -206,6 +290,7 @@ const mapDispatchToProps = (dispatch) => {
     // updateRoom : (data) => {dispatch(room(data))},
     // updateUserList : (data) => {dispatch(gamerUserList(data))},
     // updateInit : (data) => {dispatch(gamerInit(data))},
+    setMessageList: (data) => {dispatch(setMessageList(data))},
   }
 }
 
