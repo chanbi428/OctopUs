f78eedac9092e41b568264e46c4b05e517a054cf
오하민 <ohm55545@naver.com>
2022-08-12 16:46:19 +0900

    FEAT/REQ-19/OHM

--- FrontEnd/octop-us/src/components/InGame/components/JobComponents/GameResultPage.js
+++ FrontEnd/octop-us/src/components/InGame/components/JobComponents/GameResultPage.js
@@ -7,17 +7,22 @@ import './RoundComponent.css';
 
 function GameResultComponent(props) {
   let pathName = document.location.pathname.replace("/", "");
-
+  const [winTeam, setWinTeam] = useState("")
   useEffect(() => {
     axios
       .delete(`${BASE_URL}/games/end/${pathName}`)
       .then((res) => console.log(res.data))
       .catch((err) => console.log(err))
+
+    axios
+      .get(`${BASE_URL}/gamers/victory/team/${pathName}`)
+      .then((res) => setWinTeam(res.data.gameTeam))
+      .catch((err) => console.log(err))
   })
   
   return (
-      <div>
-        Game Result
+    <div>
+      {winTeam} 승리!
     </div>
   )
 }
