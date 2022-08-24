fd6c84d5ac03959c5857c31c37ee9d4f5eb4309b
JangJiseon <jsmeaningood@gmail.com>
2022-08-24 17:13:59 +0900

    appjs edit

--- FrontEnd/octop-us/src/App.js
+++ FrontEnd/octop-us/src/App.js
@@ -6,14 +6,6 @@ import LandingPage from "./components/LandingPage/LandingPage";
 import MainPage from "./components/MainPage/MainPage";
 import InGame from "./components/InGame/InGame";
 import { login } from "./features/user/userSlice";
-import SharkGame from "./components/MiniGame/SharkGame/SharkGame";
-import FishingGame from "./components/MiniGame/FishingGame/FishingGame";
-import NightToDayLoading from "./components/LoadingPage/NightToDayLoading/NightToDayLoading";
-import DayToNightLoading from "./components/LoadingPage/DayToNightLoading/DayToNightLoading";
-import MafiaCard from "./components/LoadingPage/JobCard/MafiaCard/MafiaCard";
-import NeutralCard from "./components/LoadingPage/JobCard/NeutralCard/NeutralCard";
-import CrazyCard from "./components/LoadingPage/JobCard/CrazyCard/CrazyCard";
-import DoctorCard from "./components/LoadingPage/JobCard/DoctorCard/DoctorCard";
 
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -47,14 +39,6 @@ function App() {
         <Route path="/" exact element={<LandingPage />} />
         <Route path="/main" element={<MainPage />} />
         <Route path="/:roomId" element={<InGame />} />
-        <Route path="/SharkGame" element={<SharkGame />} />
-        <Route path="/FishingGame" element={<FishingGame />} />
-        <Route path="/DayToNightLoading" element={<DayToNightLoading />} />
-        <Route path="/NightToDayLoading" element={<NightToDayLoading />} />
-        <Route path="/mafia" element={<MafiaCard />} />
-        <Route path="/neutral" element={<NeutralCard />} />
-        <Route path="/crazy" element={<CrazyCard />} />
-        <Route path="/doc" element={<DoctorCard />} />
       </Routes>
     </div>
   );
