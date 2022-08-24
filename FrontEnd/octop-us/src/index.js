30bfd0b223661f1a25ebcc6e39cdf327be4a0e7d
JangJiseon <jsmeaningood@gmail.com>
2022-08-24 17:14:29 +0900

    indexjs edit

--- FrontEnd/octop-us/src/index.js
+++ FrontEnd/octop-us/src/index.js
@@ -4,26 +4,16 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-// import { createStore } from "redux";
-// import promiseMiddleware from "redux-promise";
-// import reduxThunk from "redux-thunk";
+
 import { BrowserRouter } from "react-router-dom";
 import store from "./app/store";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      {/* <Provider
-      store={createStore(
-        reducer,
-        window.__REDUX_DEVTOOLS_EXTENSION__ &&
-          window.__REDUX_DEVTOOLS_EXTENSION__()
-      )}
-      > */}
       <Provider store={store}>
         <App />
       </Provider>
-      {/* </Provider> */}
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
